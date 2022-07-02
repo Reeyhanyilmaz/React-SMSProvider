b1d44491e5ec2697f60b8d08f33baa7556774eef
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-02 17:52:16 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from './Components/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />} />      
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ChakraProvider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <ChakraProvider>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ChakraProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
