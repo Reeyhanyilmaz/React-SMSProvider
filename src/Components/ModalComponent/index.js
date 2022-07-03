b0495c0bd8c613b8214437ca1065f91f672a7229
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-03 21:11:59 +0300

    add formik

--- src/Components/ModalComponent/index.js
+++ src/Components/ModalComponent/index.js
@@ -1,24 +1,54 @@
 import { useRef } from "react";
-import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, FormControl, FormLabel, Input,
-  ModalCloseButton, useDisclosure, Button, Select} from "@chakra-ui/react";
-  import {useSelector, useDispatch} from 'react-redux';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Select,
+} from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { useFormik } from "formik";
 
 function ModalComponent() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const initialRef = useRef(null)
-  const finalRef = useRef(null)
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialRef = useRef(null);
+  const finalRef = useRef(null);
   const providerEnums = useSelector((state) => state.provider.providerEnums);
-  const shiftEnums = providerEnums.filter(item => item.value !== 0);
+  const shiftEnums = providerEnums.filter((item) => item.value !== 0);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-   console.log('e.target', e.target);
+  const formik = useFormik({
+    initialValues: {
+      ID: 0,
+      status: true,
+      ProviderID: shiftEnums[0].value,
+      PartnerID: "",
+      BaseURL:"",
+      FromName: "",
+      Username: "",
+      Password: "",
+      VendorCode: "",
+      ApiKey: "",
+      SecretKey: "",
+      AccountSID: "",
+      AuthToken: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
-  }
-
-  return (   
+  return (
     <>
-      <Button onClick={onOpen} colorScheme="teal">Add New Provider</Button>
+      <Button onClick={onOpen} colorScheme="teal">
+        Add New Provider
+      </Button>
       <Modal
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
@@ -28,81 +58,152 @@ function ModalComponent() {
         <ModalOverlay />
         <ModalContent>
           <ModalCloseButton />
-          <form onSubmit={handleSubmit}>
-            <ModalBody pb={6}>       
-              <FormControl isRequired mb="2">
-              <FormLabel>Provider</FormLabel>
-              <Select>
-                {shiftEnums.map((item, i) => (
-                <option key={i} value={item.value} >{item.name}</option>
-                ))}
-              </Select>
+          <form onSubmit={formik.handleSubmit}>
+            <ModalBody pb={6}>
+              <FormControl isRequired  mb="2">
+                <FormLabel>Provider</FormLabel>
+                <Select size="xs" onChange={formik.handleChange} name="ProviderID" >
+                  {shiftEnums.map((item, i) => (
+                    <option key={i} value={item.value}>
+                      {item.name}
+                    </option>
+                  ))}
+                </Select>
               </FormControl>
-             
-               <FormControl isRequired>
-              <FormLabel>PartnerID</FormLabel>
-               <Input size="sm"  type="number" ref={initialRef} placeholder='PartnerID'  />
-            </FormControl>
 
-               <FormControl isRequired>
-              <FormLabel>BaseURL</FormLabel>
-               <Input size="sm" type="text"  placeholder='BaseURL' />
-            </FormControl>
+              <FormControl isRequired >
+                <FormLabel>PartnerID</FormLabel>
+                <Input
+                  name="PartnerID"
+                  value={formik.values.PartnerID}
+                  size="xs"
+                  type="number"
+                  ref={initialRef}
+                  placeholder="PartnerID"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-               <FormControl isRequired mt={4}>
-              <FormLabel>FromName</FormLabel>
-               <Input size="sm" placeholder='FromName' />
-            </FormControl>
+              <FormControl isRequired >
+                <FormLabel>BaseURL</FormLabel>
+                <Input
+                  name="BaseURL"
+                  value={formik.values.BaseURL}
+                  size="xs"
+                  type="text"
+                  placeholder="BaseURL"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-              <FormControl isRequired mt={4}>
-              <FormLabel>Username</FormLabel>
-               <Input size="sm" placeholder='Username' />
-            </FormControl>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>FromName</FormLabel>
+                <Input
+                  name="FromName"
+                  value={formik.values.FromName}
+                  size="xs"
+                  type="text"
+                  placeholder="FromName"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-               <FormControl isRequired mt={4}>
-              <FormLabel>Password</FormLabel>
-               <Input size="sm" type="password" placeholder='Password' />
-            </FormControl>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  name="Username"
+                  value={formik.values.Username}
+                  size="xs"
+                  type="text"
+                  placeholder="Username"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
+              <FormControl isRequired  mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  name="Password"
+                  value={formik.values.Password}
+                  size="xs"
+                  type="password"
+                  placeholder="Password"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-               <FormControl isRequired mt={4}>
-              <FormLabel>VendorCode</FormLabel>
-               <Input size="sm" placeholder='VendorCode' />
-            </FormControl>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>VendorCode</FormLabel>
+                <Input
+                  name="VendorCode"
+                  value={formik.values.VendorCode}
+                  size="xs"
+                  type="text"
+                  placeholder="VendorCode"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-               <FormControl isRequired mt={4}>
-              <FormLabel>ApiKey</FormLabel>
-               <Input size="sm" placeholder='ApiKey' />
-            </FormControl>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>ApiKey</FormLabel>
+                <Input
+                  name="ApiKey"
+                  value={formik.values.ApiKey}
+                  size="xs"
+                  type="text"
+                  placeholder="ApiKey"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-               <FormControl isRequired mt={4}>
-              <FormLabel>SecretKey</FormLabel>
-               <Input size="sm" placeholder='SecretKey' />
-            </FormControl>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>SecretKey</FormLabel>
+                <Input
+                  name="SecretKey"
+                  value={formik.values.SecretKey}
+                  size="xs"
+                  type="text"
+                  placeholder="SecretKey"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-               <FormControl isRequired mt={4}>
-              <FormLabel>AccountSID</FormLabel>
-               <Input size="sm" placeholder='AccountSID' />
-            </FormControl>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>AccountSID</FormLabel>
+                <Input
+                  name="AccountSID"
+                  value={formik.values.AccountSID}
+                  size="xs"
+                  type="text"
+                  placeholder="AccountSID"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
 
-               <FormControl isRequired mt={4}>
-              <FormLabel>AuthToken</FormLabel>
-               <Input size="sm" placeholder='AuthToken' />
-            </FormControl>  
-                 
-          </ModalBody>
+              <FormControl isRequired  mt={4}>
+                <FormLabel>AuthToken</FormLabel>
+                <Input
+                  name="AuthToken"
+                  value={formik.values.AuthToken}
+                  size="xs"
+                  type="text"
+                  placeholder="AuthToken"
+                  onChange={formik.handleChange}
+                />
+              </FormControl>
+            </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme='teal' mr={3} type="submit" >
-              Add
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </form> 
+            <ModalFooter>
+              <Button colorScheme="teal" mr={3} type="submit">
+                Add
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
 
 export default ModalComponent;
