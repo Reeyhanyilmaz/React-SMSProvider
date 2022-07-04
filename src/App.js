be24e4c5093443f8a27915cce8a7328ee3a0421f
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-04 19:45:05 +0300

    add Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Login from './Components/Login';
 import TableList from './Components/TableList';
 import Error404 from './Components/Error404';
+import Navbar from './Components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Login />} />      
         <Route path="/tableList" element={<TableList />} />     
